8f887ecf50e56437f2038f7285f2aa433cb9cf98
Rodrigo Blefari Gonçalves <rodrigo.blefari@gmail.com>
2018-10-16 15:09:58 -0300

    alteracao-basica

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="js/script.js"></script>
+    <link rel="stylesheet" href="css/style.css">
+    <title>Boleto Banco do Brasill</title>
+</head>
+<body>
+
+    <form action=""></form>
+
+
+    
+    <style>
+        .container{
+            background-color: red;
+        }
+    </style>
+<div class="container">
+    <form action="">
+        <label for="">Nome</label>
+        <input type="text">
+        <label for="">Nome</label>
+        <input type="text">
+        <label for="">Nome</label>
+        <input type="text">
+    </form>
+</div>
+</body>
+</html>''
