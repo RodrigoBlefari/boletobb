5ba72af32fbc8fad3d9d2cbba1b7af93d648dcda
Rodrigo Blefari Gonçalves <rodrigo.blefari@gmail.com>
2018-10-16 17:19:11 -0300

    Personalizacao html

--- index.php
+++ index.php
@@ -10,22 +10,27 @@
 </head>
 <body>
 
-    <form action=""></form>
-
 <div class="container">
-
     <form action="">
     <div class="body-form">
     <h1>GERADOR DE BOLETO<br> BANCO DO BRASIL</h1>
     </div>
-    
+    <h2>GERAR TOKEN</h2>
+        <label for=""> <input placeholder="TOKEN" type="text"></label>    
+        <input class="token-generator" type="button" value="GERAR TOKEN">
+    </form>
+</div>
+
+<div class="container">
+    <form action="">
+  
+    <h2>GERADOR DE BOLETO </h2>
+        <label for=""> <input placeholder="TOKEN" type="text"></label>
         <label for=""> <input placeholder="NOME" type="text"></label>
         <label for=""> <input placeholder="NOME" type="text"></label>
         <label for=""> <input placeholder="NOME" type="text"></label>
         <label for=""> <input placeholder="NOME" type="text"></label>
-        <label for=""> <input placeholder="NOME" type="text"></label>
-        <input type="button" value="GERAR BOLETO">
-    
+        <input class="bol-generator" type="button" value="GERAR BOLETO">
     </form>
 </div>
 </body>
